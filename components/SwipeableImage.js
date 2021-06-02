3052d048520c0c7427737d55e23f32367f880c3c
ronybarua.ethical18 <ronybarua.ethical18@gmail.com>
2021-06-03 00:22:58 +0600

    Name and age displayed

--- components/SwipeableImage.js
+++ components/SwipeableImage.js
@@ -1,10 +1,16 @@
 import React from 'react'
-import { View, Image, StyleSheet } from 'react-native'
+import { View, Image, StyleSheet, Text } from 'react-native'
 
 export default function SwipeableImage({user}) {
     return (
         <View>
             <Image src={{uri: user.picture.large}} style={styles.photo}></Image>
+            <View style={styles.textContainer}>
+                <View style={style.textRow}>
+                    <Text>{user.name.first}</Text>
+                    <Text>{user.dob.age}</Text>
+                </View>
+            </View>
         </View>
     )
 
@@ -14,5 +20,14 @@ const styles = StyleSheet.create({
         height: '100%',
         resizeMode: 'cover',
         borderRadius: 20
+    },
+    textContainer:{
+        position: 'absolute',
+        bottom:20,
+        left:20
+    },
+    textRow:{
+        flexDirection: 'row',
+        alignItems:center
     }
 })
