66d49a45f2fa853b17babeb01debf422df080b22
ronybarua.ethical18 <ronybarua.ethical18@gmail.com>
2021-06-04 13:02:22 +0600

    fetched data displayed on UI

--- App.js
+++ App.js
@@ -1,15 +1,16 @@
 import React, { useEffect, useState } from 'react';
-import { Alert, StyleSheet, Text, View } from 'react-native';
+import { Alert, StyleSheet, View, Text } from 'react-native';
 import axios from 'axios'
 import SwipeableImage from './components/SwipeableImage';
+import Constants from 'expo-constants'
+import TopBar from './components/TopBar';
 export default function App() {
   const [users, setUsers] = useState([]);
   const [currentIndex, setCurrentIndex] = useState(0);
-  const fetchRandomUsers = async () =>{
+  async function fetchRandomUsers(){
     try {
       const {data} = await axios.get('https://randomuser.me/api/?gender=female&results=50')
       setUsers(data.results);
-      console.log(users)
     } catch (error) {
       console.log('Something went wrong while fetching data')
       Alert.alert('Error fetching users', '', [{text: 'Retry', onPress: () => fetchRandomUsers()}])
@@ -20,8 +21,9 @@ export default function App() {
   }, [])
   return (
     <View style={styles.container}>
+      <TopBar></TopBar>
       <View style={styles.swipes}>
-          {users.length > 1 && (<SwipeableImage user={users[currentIndex]} />)}
+          {users.length > 1 && (<SwipeableImage user={users[currentIndex]}/>)}
       </View>
     </View>
   );
@@ -30,13 +32,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop:Constants.statusBarHeight
   },
   swipes:{
     flex:1,
-    paddingTop:10,
+    paddingTop:20,
     shadowColor:'#000',
     shadowOffset:{
       width:0,
